804562011356b0ed806cee60db5d72ec38561c35
cristian.morales@bnamericas.com <estoybien>
2021-07-30 01:10:16 -0400

    count terms ok

--- src/main/java/cl/binter/challenge/usecase/TextFinder.java
+++ src/main/java/cl/binter/challenge/usecase/TextFinder.java
@@ -1,25 +1,42 @@
 package cl.binter.challenge.usecase;
 
+import cl.binter.challenge.domain.entities.ScoreTerm;
 import cl.binter.challenge.domain.entities.Text;
 import cl.binter.challenge.domain.repository.TextRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
+/**
+ * Text Finder Service
+ */
 @Service
 @Slf4j
 public class TextFinder {
 
+	/**
+	 * Text repository
+	 */
 	private TextRepository textRepository;
 
-	public Text getText(Long id, Integer page) {
-		if(Objects.isNull(page)){
-			if(Objects.isNull(id)){
+	@Autowired
+	public void setTextRepository(TextRepository textRepository) {
+		this.textRepository = textRepository;
+	}
+
+
+	public Text getText(Long id, Integer page)  {
+		if (Objects.isNull(page)) {
+			if (Objects.isNull(id)) {
 				return getRandomText();
 			}
 			return textRepository.findText(id);
@@ -28,16 +45,45 @@ public class TextFinder {
 	}
 
 	public Text getRandomText() {
-		List<Long> allIds = textRepository.findAllIds();
-		Random rnd = new Random();
-		Integer index = rnd.nextInt(allIds.size());
-
+		var allIds = textRepository.findAllIds();
+		var index = new Random().nextInt(allIds.size());
 		return textRepository.findText(allIds.get(index));
 	}
 
+	/**
+	 * Score Term method
+	 *
+	 * @param id id doc
+	 * @param page page of doc
+	 * @return List of Score term
+	 */
+	public List<ScoreTerm> scoreTerm(Long id, Integer page) {
+		var text = textRepository.findText(id, page);
+		return buildCountTerm(text.getTextBlock());
+	}
 
-	@Autowired
-	public void setTextRepository(TextRepository textRepository) {
-		this.textRepository = textRepository;
+	/**
+	 * Build count list of terms
+	 *
+	 * @param texBlock textblock to evaluate
+	 * @return count list of terms
+	 */
+	private List<ScoreTerm> buildCountTerm(String texBlock) {
+		if (Objects.nonNull(texBlock) && texBlock.length() > 0) {
+			List<String> terms = Arrays
+					.stream(texBlock.replaceAll("[^\\dA-Za-z ]", "").toLowerCase()
+					.split(" "))
+					.collect(Collectors.toList());
+			var termsSet = new HashSet<>(terms);
+			var resultSet = new HashSet<ScoreTerm>();
+			termsSet.forEach(term ->
+					resultSet.add(new ScoreTerm(term, (int) terms.stream().filter(term::equals).count()))
+			);
+			return resultSet.stream()
+					.sorted(Comparator.comparing(ScoreTerm::getMatches)
+					.reversed())
+					.collect(Collectors.toList());
+		}
+		return new ArrayList<>();
 	}
 }
--- src/test/java/cl/binter/challenge/usecase/TextFinderTest.java
+++ src/test/java/cl/binter/challenge/usecase/TextFinderTest.java
@@ -1,5 +1,6 @@
 package cl.binter.challenge.usecase;
 
+import cl.binter.challenge.domain.entities.ScoreTerm;
 import cl.binter.challenge.domain.entities.Text;
 import cl.binter.challenge.domain.repository.TextRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,9 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -17,15 +21,29 @@ public class TextFinderTest {
     @Mock
     private TextRepository textRepository;
 
+    @InjectMocks
+    private TextFinder textFinder;
+
     @BeforeEach
     void setup() {
     }
 
     @Test
     void matches_success() {
-        var text = Text.builder()
-                .textBlock("Esta es una prueba, donde encontraremos las palabras repetidas").build();
-        when(textRepository.findText(10L, 1)).thenReturn(text);
-
+        var textBlock = "Así pues, el doctor era hombre bien conocido, pese a no pertenecer a ninguna institución " +
+                "científica, ni a las Reales Sociedades Geográficas de Londres, París, Berlín, Viena o San Petersburgo," +
+                " ni al Club de los Viajeros, ni siquiera a la Royal Politechnic Institution, donde su amigo, el " +
+                "estadista Kokburn, metía mucho ruido. Un día Kokburn le propuso, para darle gusto, resolver el " +
+                "siguiente problema: dado el número de millas recorridas por el doctor alrededor del mundo, cuántas " +
+                "millas más ha andado su cabeza que sus pies, teniendo en cuenta la diferencia de los radios O bien, " +
+                "conociendo el número de millas recorridas por los pies y por la cabeza del doctor, calcular su " +
+                "estatura con toda exactitud.";
+        var text = Text.builder().textBlock(textBlock).build();
+        when(textRepository.findText(1L, 3)).thenReturn(text);
+        List<ScoreTerm> result = textFinder.scoreTerm(1L, 3);
+        assertEquals(83, result.size());
+        assertEquals(6, result.stream()
+                .filter(term -> term.getTerm().equals("el"))
+                .findFirst().get().getMatches());
     }
 }
